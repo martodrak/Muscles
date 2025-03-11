8c058d30d1659870f9031a4646f0803450295710
martodrak <martodrak@gmail.com>
2025-03-12 00:47:13 +0100

    Úprava obrázků

--- src/parts/Memory.jsx
+++ src/parts/Memory.jsx
@@ -25,14 +25,14 @@ export const Memory = ({ children }) => {
       latin: "Abdominals",
       description:
         "Skládá se z rectus abdominis, obliques (šikmé svaly) a transversus abdominis. Aktivuje se při flexi trupu a stabilizaci pánve. Cvičení: crunches, plank, ruské rotace. Pohyb: ohyb trupu nebo rotace. Pohybové směry: flexe trupu, rotace a laterální flexe.",
-      image: "images/bricho.jpg",
+      image: "images/panak_bricho.png",
     },
     {
       name: "Hýžďové svaly",
       latin: "Gluteus Maximus, Medius, Minimus",
       description:
         "Má tři části: Gluteus maximus – největší a nejmocnější část, Gluteus medius a minimus – stabilizace pánve. Aktivuje se při zvedání pánve a extenzi kyčlí. Cvičení: dřepy, výpady, hip thrusty. Pohyb: zvedání pánve nebo nohy směrem vzhůru.",
-      image: "images/stehna.jpg",
+      image: "images/zadek.jpg",
     },
     {
       name: "Lýtkový sval",
@@ -46,28 +46,28 @@ export const Memory = ({ children }) => {
       latin: "Forearm Muscles",
       description:
         "Rozdělen na flexory a extenzory. Aktivuje se při stiskových a rotačních pohybech. Cvičení: zdvihy činek, prsty na kladce. Pohyb: flexe a extenze zápěstí a prstů. Pohybové směry: flexe a extenze v zápěstí.",
-      image: "images/predlokti.jpg",
+      image: "images/panak_predlokti.png",
     },
     {
       name: "Prsní sval",
       latin: "Pectoralis Major",
       description:
         "Má dvě hlavní části: klíčkovou a sternální část. Prsní svaly se aktivují při tlačících pohybech. Cvičení: bench press, tlak s jednoručkami. Pohyb: stlačení paží směrem k sobě. Pohybové směry: horizontální (při bench pressu) a vertikální (při tlaku nad hlavou).",
-      image: "images/prsa.jpg",
+      image: "images/animace1panak.png",
     },
     {
       name: "Ramenní sval",
       latin: "Deltoideus",
       description:
         "Má tři části: Přední – zvedání paže dopředu, Střední – zvedání paže do stran, Zadní – zvedání paže dozadu. Aktivuje se při tlakových a zvedacích pohybech. Cvičení: shoulder press, boční a přední zdvihy. Pohybové směry: přední, boční a zadní pohyb.",
-      image: "images/ramena.jpg",
+      image: "images/panak_ramena.png",
     },
     {
-      name: "Stehenní sval",
+      name: "Stehenní přední sval",
       latin: "Quadriceps",
       description:
         "Má čtyři části: Rectus femoris, vastus medialis, vastus lateralis, vastus intermedius. Aktivuje se při prodlužování kolene. Cvičení: dřepy, leg press, výpady. Pohyb: extenze kolene.",
-      image: "images/stehna.jpg",
+      image: "images/panak_stehna.png",
     },
     {
       name: "Tricepsový sval",
@@ -82,6 +82,13 @@ export const Memory = ({ children }) => {
       description:
         "Skládá se z mnoha svalů, například trapezy, latisimy atp. Aktivuje se při tahání. Cvičení: shyby, přítahy v předklonu, stahování kladky. Pohyb: zvedání paží a přitahování rukou směrem k tělu. Pohybové směry: vertikální (při shybování) a horizontální (při přítazích).",
       image: "images/zada.jpg",
+    },
+    {
+      name: " Stehení zadní sval",
+      latin: "Deltoideus",
+      description:
+        "Má tři části: Přední – zvedání paže dopředu, Střední – zvedání paže do stran, Zadní – zvedání paže dozadu. Aktivuje se při tlakových a zvedacích pohybech. Cvičení: shoulder press, boční a přední zdvihy. Pohybové směry: přední, boční a zadní pohyb.",
+      image: "images/ramena.jpg",
     },
   ];
 
