cf3a4587f6943f4621c69232d50eee32ce004da4
xxKuzi <jakub.kuzel7@gmail.com>
2025-07-16 16:09:46 +0200

    footer + hosting update

--- src/parts/Footer.jsx
+++ src/parts/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="flex items-center mt-[1d00px] bg-gray-200 h-12 justify-end py-2 px-4">
+    <div className="flex items-center mt-[1d00px] bg-gray-100 h-12 justify-end py-2 px-4">
       <p className="">Developed by Martin Drinka</p>
     </div>
   );
