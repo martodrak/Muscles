b886b7ef658a7aae8847cab4a54167fd84a71337
xxKuzi <jakub.kuzel7@gmail.com>
2025-07-22 15:04:59 +0200

    bigger video player for desktop

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const VideoPlayer = ({ videoLink }) => {
   return (
-    <div className="flex flex-col items-center justify-center lg:w-[60vw] md:w-[70vw] w-[80vw] bg-gray-100 p-4 rounded-xl">
+    <div className="flex flex-col items-center justify-center lg:w-[60vw] w-[80vw] bg-gray-100 p-4 rounded-xl">
       {/* <h1 className="text-3xl font-bold mb-4">Video ukázka</h1> */}
-      <div className="w-full max-w-2xl aspect-video">
+      <div className="w-full max-w-5xl aspect-video">
         <iframe
           className="w-full h-full rounded shadow-lg"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ" //videoLink - previous
+          src={videoLink}
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
